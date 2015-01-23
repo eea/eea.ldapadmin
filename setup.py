5669f647088f257f4df9c77b72cbd93785f717fd
Tiberiu Ichim <tiberiu.ichim@eaudeweb.ro>
2015-01-23 11:38:16 +0000

    Bump egg to development

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='eea.ldapadmin',
-      version='1.4.65',
+      version='1.4.66',
       author='Eau de Web',
       author_email='office@eaudeweb.ro',
       packages=find_packages(),
