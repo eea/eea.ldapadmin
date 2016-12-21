eb224f3937a042479fc19c14daedb6ec39d6ff23
DragosChirila <dragos.chirila@eaudeweb.ro>
2016-12-21 17:59:40 +0200

    Task: API entrypoint for EIONET LDAP command line scripts   - no need to use an access token   [chiridra #80115]

--- eea/ldapadmin/api_tool.py
+++ eea/ldapadmin/api_tool.py
@@ -1,13 +1,11 @@
 """ This module provides support for calling various methods via http.
-    A token is required to validate a request.
 """
 
 import logging
 
 from AccessControl import ClassSecurityInfo
-from AccessControl.Permissions import view
+from AccessControl.Permissions import view, view_management_screens
 from App.class_init import InitializeClass
-from OFS.PropertyManager import PropertyManager
 from OFS.SimpleItem import SimpleItem
 
 from eea.ldapadmin.constants import LDAP_DISK_STORAGE
@@ -29,8 +27,7 @@ def manage_add_api_tool(parent, REQUEST=None):
         REQUEST.RESPONSE.redirect(parent.absolute_url() + '/manage_workspace')
 
 
-class ApiTool(PropertyManager,
-    SimpleItem):
+class ApiTool(SimpleItem):
     """ ApiTool class
     """
 
@@ -39,48 +36,31 @@ class ApiTool(PropertyManager,
     icon = '++resource++eea.ldapadmin-www/eionet_password_reset_tool.gif'
 
     manage_options = (
-        PropertyManager.manage_options
-        +
         SimpleItem.manage_options
-    )
-
-    _properties = (
-        PropertyManager._properties
-        +
-        (
-            {'id': 'token', 'type': 'string', 'mode': 'w'},
-        )
     )
 
     def __init__(self, id, title):
         super(ApiTool, self).__init__()
         self.id = id
         self.title = title
-        self.token = ''
 
-    def update_countries(self, token='', REQUEST=None, RESPONSE=None):
+    security.declareProtected(view_management_screens, 'update_countries')
+    def update_countries(self, REQUEST=None, RESPONSE=None):
         """ Wrapper for update_countries.
         """
-        if self.token == token.strip():
-            update_countries()
-            return 'FINISHED update_countries @ {}/'.format(
-                self.absolute_url()
-            )
-        else:
-            RESPONSE.setStatus(404)
-            return RESPONSE
+        update_countries()
+        return 'FINISHED update_countries @ {}/'.format(
+            self.absolute_url()
+        )
 
-    def dump_ldap(self, token='', REQUEST=None, RESPONSE=None):
+    security.declareProtected(view_management_screens, 'dump_ldap')
+    def dump_ldap(self, REQUEST=None, RESPONSE=None):
         """ """
-        if self.token == token.strip():
-            dump_ldap(LDAP_DISK_STORAGE)
-            return 'FINISHED dump_ldap @ {}/ to {}'.format(
-                self.absolute_url(),
-                LDAP_DISK_STORAGE
-            )
-        else:
-            RESPONSE.setStatus(404)
-            return RESPONSE
+        dump_ldap(LDAP_DISK_STORAGE)
+        return 'FINISHED dump_ldap @ {}/ to {}'.format(
+            self.absolute_url(),
+            LDAP_DISK_STORAGE
+        )
 
     security.declareProtected(view, 'index_html')
     def index_html(self, REQUEST=None, RESPONSE=None):
