d1834f1d2bbf6d1afea2d5b92b2c5a1043742308
Cornel Nițu <cornel.nitu@eaudeweb.ro>
2017-02-07 16:26:51 +0200

    allow temporary import of userid and password

--- eea/ldapadmin/users_admin.py
+++ eea/ldapadmin/users_admin.py
@@ -1154,13 +1154,23 @@ class BulkUserImporter(BrowserView):
     """ A view to bulk import users from an xls file
     """
     buttons = ('download_template', 'bulk_create')
-    TEMPLATE_COLUMNS = ["First Name*", "Last Name*",
-                        "Full name (native language)",
-                        "Search helper (ASCII characters only!)",
-                        "E-mail*", "Job Title", "URL", "Postal Address",
-                        "Telephone Number*", "Mobile Telephone Number",
-                        "Fax Number", "Organisation*", "Department",
-                        "Reason to create*"]
+    TEMPLATE_COLUMNS = [
+        "User ID",
+        "Password",
+        "First Name*",
+        "Last Name*",
+        "Full name (native language)",
+        "Search helper (ASCII characters only!)",
+        "E-mail*",
+        "Job Title",
+        "URL",
+        "Postal Address",
+        "Telephone Number*",
+        "Mobile Telephone Number",
+        "Fax Number",
+        "Organisation*",
+        "Department",
+        "Reason to create*"]
 
     def __call__(self):
         """ upload view """
@@ -1210,11 +1220,11 @@ class BulkUserImporter(BrowserView):
                 properties[column.lower()] = value
 
             row_data = excel_headers_to_object(properties)
-            row_data['password'] = generate_password()
-            row_data['id'] = generate_user_id(row_data['first_name'],
-                                              row_data['last_name'],
-                                              agent, id_list)
-            id_list.append(row_data['id'])
+            # row_data['password'] = generate_password()
+            # row_data['id'] = generate_user_id(row_data['first_name'],
+            #                                   row_data['last_name'],
+            #                                   agent, id_list)
+            #id_list.append(row_data['id'])
             row_data['url'] = process_url(row_data['url'])
             result.append(row_data)
 
